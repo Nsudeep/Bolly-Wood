fb82d0ee5512ba55259b2e7049fbc0d83da8870b
Nsudeep <1994sudeep@gmail.com>
2016-12-29 18:24:36 -0800

    Completed the skeleton of userInputActivity

--- app/src/main/java/com/example/tejas/bolly_wood/userInputActivity.java
+++ app/src/main/java/com/example/tejas/bolly_wood/userInputActivity.java
@@ -18,7 +18,13 @@ public class userInputActivity extends AppCompatActivity {
     private String maskedMovie;
     private Button enterMovie;
     private Button continueButton;
-
+    private TextView aiDecisionText;
+    private EditText userPositionInput;
+    private Button positionAccepted;
+    private TextView message;
+    private String guessesMadeString = "Incorrect Guesses:";
+    private TextView incorrectGuesses;
+    private char userLetter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,12 @@ public class userInputActivity extends AppCompatActivity {
         movieInput = new EditText(this);
         movieInput.setId(20);
         enterMovie = new Button(this);
+        aiDecisionText = new TextView(this);
+        aiDecisionText.setId(40);
+        userPositionInput = new EditText(this);
+        maskedMovieText = new TextView(this);
+        maskedMovieText.setId(21);
+        maskedMovieText.setTextSize(22);
         enterMovie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,16 +56,70 @@ public class userInputActivity extends AppCompatActivity {
         });
 
         continueButton = new Button(this);
+        continueButton.setId(30);
         continueButton.setText("Continue");
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                userLetter = 'A';
+                aiDecisionText.setText("The AI chose letter"+ userLetter +"  and enter positions. Press N for an incorrect guess");
+                userPositionInput.setVisibility(View.VISIBLE);
+                positionAccepted.setVisibility(View.VISIBLE);
                 return;
             }
         });
-        maskedMovieText = new TextView(this);
-        maskedMovieText.setId(21);
-        maskedMovieText.setTextSize(22);
+        positionAccepted = new Button(this);
+        positionAccepted.setText("OK");
+        positionAccepted.setId(40);
+        positionAccepted.setVisibility(View.INVISIBLE);
+        message = new TextView(this);
+        message.setId(60);
+        positionAccepted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = userPositionInput.getText().toString();
+                System.out.println("input is " + input.charAt(0));
+                String[] strArr = input.split(",");
+                System.out.println("gfhf" + strArr[0]);
+                int len = strArr.length;
+                int[] intArray = new int[len];
+                int num, flag=0;
+                System.out.println("The 0thcharacter is");
+                System.out.println(strArr[0].charAt(0));
+                if((strArr[0].charAt(0) == 'N') || (strArr[0].charAt(0) == 'n'))
+                {
+                    flag = 1;
+                    char incorrectGuess = 'B';
+                    guessesMadeString += incorrectGuess;
+                    incorrectGuesses.setText(guessesMadeString);
+                }
+                for(int i = 0; i < len && flag== 0; i++) {
+
+                    intArray[i] = Integer.parseInt(strArr[i]);
+                    num = intArray[i];
+                    StringBuilder mm = new StringBuilder(maskedMovie);
+                    mm.setCharAt(num-1, userLetter);
+                    maskedMovie = mm.toString();
+                }
+
+                maskedMovieText.setText(maskedMovie);
+
+                if(maskedMovie.indexOf('_') == -1) {
+                    message.setText("The AI was able to successfully guess the movie");
+                    positionAccepted.setVisibility(View.INVISIBLE);
+                    userPositionInput.setVisibility(View.INVISIBLE);
+                }
+
+                userPositionInput.setText("");
+                InputMethodManager imm = (InputMethodManager) getSystemService(giveMovie.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);// to hide the keyboard
+                return;
+            }
+        });
+        incorrectGuesses = new TextView(this);
+        incorrectGuesses.setText(guessesMadeString);
+        incorrectGuesses.setId(50);
+
         enterMovie.setText("ENTER");
         Resources r = getResources();
         int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,r.getDisplayMetrics());
@@ -76,7 +142,8 @@ public class userInputActivity extends AppCompatActivity {
         movieInput.setVisibility(View.VISIBLE);
         enterMovie.setVisibility(View.VISIBLE);
 
-        setContentView(userInputLayout);
+        //setContentView(userInputLayout);
+        maskedMovieText.setWidth(px);
 
         RelativeLayout.LayoutParams maskedMovieTextDetails = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
@@ -93,6 +160,54 @@ public class userInputActivity extends AppCompatActivity {
         continueButtonDetails.addRule(RelativeLayout.ALIGN_BOTTOM,maskedMovieText.getId());
         userInputLayout.addView(continueButton);
         continueButton.setVisibility(View.INVISIBLE);
+
+        RelativeLayout.LayoutParams aiDecisionTextDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        aiDecisionTextDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        aiDecisionTextDetails.addRule(RelativeLayout.BELOW,continueButton.getId());
+        userInputLayout.addView(aiDecisionText,aiDecisionTextDetails);
+
+        userPositionInput.setVisibility(View.INVISIBLE);
+        userPositionInput.setWidth(px);
+
+        RelativeLayout.LayoutParams userPositionInputDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        userPositionInputDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        userPositionInputDetails.addRule(RelativeLayout.BELOW,aiDecisionText.getId());
+        userInputLayout.addView(userPositionInput,userPositionInputDetails);
+
+        RelativeLayout.LayoutParams positionAcceptedDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        positionAcceptedDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+        positionAcceptedDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        //positionAcceptedDetails.addRule(RelativeLayout.BELOW,userPositionInput.getId());
+        userInputLayout.addView(positionAccepted,positionAcceptedDetails);
+
+        RelativeLayout.LayoutParams messageDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        messageDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        messageDetails.addRule(RelativeLayout.BELOW,aiDecisionText.getId());
+        userInputLayout.addView(message,messageDetails);
+
+        RelativeLayout.LayoutParams incorrectGuessesDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        incorrectGuessesDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        incorrectGuessesDetails.addRule(RelativeLayout.BELOW,message.getId());
+        incorrectGuessesDetails.addRule(RelativeLayout.ALIGN_BOTTOM,message.getId());
+        userInputLayout.addView(incorrectGuesses,incorrectGuessesDetails);
+
+        setContentView(userInputLayout);
+
 
 
     }
